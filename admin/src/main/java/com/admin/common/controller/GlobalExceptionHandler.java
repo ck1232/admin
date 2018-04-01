b70ca90c0b3f6a3d0bf86ad4459ebb29e0ebafcd
Choon Keat <behchoonkeat@hotmail.com>
2018-04-01 23:52:15 +0800

    query and logs

--- admin/src/main/java/com/admin/common/controller/GlobalExceptionHandler.java
+++ admin/src/main/java/com/admin/common/controller/GlobalExceptionHandler.java
@@ -1,12 +1,11 @@
 package com.admin.common.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,28 +13,14 @@ public class GlobalExceptionHandler {
 	private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
-	public String exceptionHandler(Exception e) {
-		logger.error("Error caught by global exception handler", e);
+	public String exceptionHandler(HttpServletRequest request, Exception e) {
+		logger.error("An Error occured for Method :"+ request.getMethod() + "not supported for URL:"+ request.getRequestURL(), e);
 		return "pageError";
 	}
 	
-	@ExceptionHandler(NoHandlerFoundException.class)
-	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public String handleNotFoundException(Exception e) {
-        logger.error("A null pointer exception ocurred " , e);
-        return "pageNotFound";
-    }
-	
-	@ExceptionHandler(javax.el.PropertyNotFoundException.class)
-	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public String handleErrorException(Exception e) {
-        logger.error("An exception ocurred " , e);
-        return "pageError";
-    }
-	
-	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-	public String handleMethodNotAllowedException(Exception e) {
-        logger.error("A null pointer exception ocurred " , e);
+	@ExceptionHandler(org.springframework.web.HttpRequestMethodNotSupportedException.class)
+	public String methodNotSupportedException(HttpServletRequest request, Exception e) {
+        logger.error("Method :"+ request.getMethod() + "not supported for URL:"+ request.getRequestURL(), e);
         return "pageError";
     }
 }
